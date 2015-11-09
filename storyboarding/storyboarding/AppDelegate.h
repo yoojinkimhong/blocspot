000ec3889169ea6694d1ef1baa219f5bca0cd511
YooJ <yoojinkimhong@gmail.com>
2015-11-09 14:22:26 -0800

    new storyboarding file

---
+++ storyboarding/storyboarding/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  storyboarding
+//
+//  Created by YooJin Hong on 11/9/15.
+//  Copyright © 2015 YooJin Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
